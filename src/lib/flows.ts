8772f52272e4aefe29b423b1d2fa0673f828138b
rupak-stack <rupak@frejun.com>
2026-07-16 13:21:30 +0530

    js docs

--- src/lib/flows.ts
+++ src/lib/flows.ts
@@ -1,20 +1,18 @@
 export class CallFlow {
+    /**
+     * Build and return stream action flow.
+     * 
+     * @params:
+     * 1. wsUrl: Remote WebSocket URL
+     * 2. sampleRate: Sample rate of Teler audio
+     * 3. chunkSize: Chunk size of Teler audio
+     * 4. record: Recording required
+     * 
+     * @returns
+     * 1. JSON response contains the stream details
+     * 
+     */
     static stream(wsUrl: string, options: { sampleRate?: string, chunkSize?: number, record?: boolean } = {}) {
-        
-        /**
-         * Build and return stream action flow.
-         * 
-         * @params:
-         * 1. wsUrl: Remote WebSocket URL
-         * 2. sampleRate: Sample rate of Teler audio
-         * 3. chunkSize: Chunk size of Teler audio
-         * 4. record: Recording required
-         * 
-         * @returns
-         * 1. JSON response contains the stream details
-         * 
-         */
-
         return {
             action: "stream",
             ws_url: wsUrl,
@@ -24,35 +22,31 @@ export class CallFlow {
         };
     }
 
+    /**
+     * Build and return play action flow
+     * 
+     * @param:
+     * 1. mediaUrl: URL of the audio to be played.
+     * 
+     * @returns
+     * 1. JSON response contains the play details
+     * 
+     */
     static play(mediaUrl: string) {
-
-        /**
-         * Build and return play action flow
-         * 
-         * @param:
-         * 1. mediaUrl: URL of the audio to be played.
-         * 
-         * @returns
-         * 1. JSON response contains the play details
-         * 
-         */
-
         return {
             action: "play",
             media_url: mediaUrl,
         };
     }
 
+    /**
+     * Build and return hangup action flow
+     * 
+     * @returns
+     * 1. JSON response contains the hangup details
+     * 
+     */
     static hangup() {
-
-        /**
-         * Build and return hangup action flow
-         * 
-         * @returns
-         * 1. JSON response contains the hangup details
-         * 
-         */
-
         return {
             "action": "hangup",
         }
